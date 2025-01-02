033ad4004403d4c075d710690a5de429918cf1b6
mikolaj <smol.mikolaj@gmail.com>
2025-01-02 20:50:13 +0100

    Przyklad dodania do bazy danych

--- api/models.py
+++ api/models.py
@@ -10,3 +10,11 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title
+
+class Travel(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
--- myfrontend/src/App.js
+++ myfrontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import TaskList from './components/TaskList';
 
+
 function App() {
     return (
         <div className="App">
