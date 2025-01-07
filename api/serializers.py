51a52e50fb1e5ad4b7f129c3396ece3242cea506
zoja <01178224@pw.edu.pl>
2025-01-07 20:24:32 +0100

    dodanie serializers, views i modyfikacja modeli

--- api/serializers.py
+++ api/serializers.py
@@ -1,13 +1,39 @@
 from rest_framework import serializers
-from .models import Task, Travel
-
-# Serializator przekształca dane modelu Task na JSON i odwrotnie
-class TaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Task  # Używany model
-        fields = '__all__'  # Uwzględnia wszystkie pola modelu
+from .models import (Travel, User, Cost,
+                     Item_list, Places_to_see,
+                     Travel_part, Travel_group)
 
 class TravelSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Travel  # Używany model
-        fields = '__all__'  # Uwzględnia wszystkie pola modelu
+        model = Travel
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class CostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cost
+        fields = '__all__'
+
+class Places_to_see_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Places_to_see
+        fields = '__all__'
+
+class Travel_part_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Travel_part
+        fields = '__all__'
+
+class Travel_group_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Travel_group
+        fields = '__all__'
+
+class Item_listSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item_list
+        fields = '__all__'
