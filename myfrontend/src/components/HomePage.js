f3a7110a0bc91ddd4d8feef9c525fab0f98d5323
Michal Sokala <1.michal3002@gmail.com>
2025-01-10 23:36:09 +0100

    modified paths in app.js and homepage.js files

--- myfrontend/src/components/HomePage.js
+++ myfrontend/src/components/HomePage.js
@@ -7,10 +7,10 @@ const HomePage = () => {
             <h1>Witamy w aplikacji podróżniczej</h1>
             <nav>
                 <ul>
-                    <li><Link to="/travels">Podróże</Link></li>
-                    <li><Link to="/travel-groups">Grupy podróżnicze</Link></li>
+                    <li><Link to="/travel">Podróże</Link></li>
+                    <li><Link to="/travel_groups">Grupy podróżnicze</Link></li>
                     <li><Link to="/costs">Koszty</Link></li>
-                    <li><Link to="/travel-parts">Części podróży</Link></li>
+                    <li><Link to="/travel_parts">Części podróży</Link></li>
                     <li><Link to="/items">Lista przedmiotów</Link></li>
                     <li><Link to="/places">Miejsca do zobaczenia</Link></li>
                 </ul>
