8b63c752c518620b8b2a8bcee9effe18133a3ddc
zoja <01178224@pw.edu.pl>
2025-01-13 20:19:46 +0100

    modyfikacja relacji many to many

--- api/views.py
+++ api/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from .models import (Travel, User, Cost, Item_list,
-                     Places_to_see, Travel_part, Travel_group)
-
-
-from api.serializers import (TravelSerializer, UserSerializer,
-                CostSerializer, Item_listSerializer, Places_to_see_Serializer,
-                             Travel_part_Serializer, Travel_group_Serializer)
+from .models import *
+from api.serializers import *
 
 class TravelViewSet(ModelViewSet):
     queryset = Travel.objects.all()
@@ -31,7 +26,3 @@ class Places_to_see_ViewSet(ModelViewSet):
 class Travel_part_ViewSet(ModelViewSet):
     queryset = Travel_part.objects.all()
     serializer_class = Travel_part_Serializer
-
-class Travel_group_ViewSet(ModelViewSet):
-    queryset = Travel_group.objects.all()
-    serializer_class = Travel_group_Serializer
