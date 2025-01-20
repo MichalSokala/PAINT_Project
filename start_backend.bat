e1c97ba1bba6278b1e34dde0e4bfe4e2819a7423
mikolaj <smol.mikolaj@gmail.com>
2025-01-20 16:20:25 +0100

    update bat

--- start_backend.bat
+++ start_backend.bat
@@ -9,7 +9,7 @@ echo Uruchamianie backendu Django...
 start /min cmd /c "python manage.py runserver"
 
 :: Otwórz stronę backendu w przeglądarce
-start http://127.0.0.1:8000/api/
+start http://127.0.0.1:8000/admin/
 
 echo Backend został uruchomiony.
 pause
